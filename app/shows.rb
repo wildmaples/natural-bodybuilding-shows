d490d9e12bdb1b22ddf8a57260d49350df32613f
wildmaples <maple.develops@gmail.com>
2022-03-26 15:21:46 -0400

    Merge two data sources for display

--- app/shows.rb
+++ app/shows.rb
@@ -6,6 +6,8 @@ class Shows
   end
 
   def get
-    @shows ||= YAML.load_file("wnbf_events_2022.yaml", permitted_classes: [Date])
+    wnbf = YAML.load_file("db/wnbf_events_2022-03-25.yaml", permitted_classes: [Date])["events"]
+    ocb = YAML.load_file("db/ocb_amateur_events_2022-03-25.yaml", permitted_classes: [Date])["events"]
+    @shows ||= wnbf.merge!(ocb)
   end
 end
